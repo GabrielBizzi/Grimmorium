f3cea21ddda6f805f558c845f5a597f5ec5108a1
Gabriel Bizzi <galves@pdasolucoes.com.br>
2022-02-10 17:53:20 -0300

    Finalized component MyBooks

--- src/Components/MyBooks/index.tsx
+++ src/Components/MyBooks/index.tsx
@@ -1,35 +1,48 @@
-import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
-import { ContainerLinkBook, ContainerMyBook } from './styles';
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import {
+  ContainerLinkBook,
+  ContainerMyBook,
+  ImageBook,
+  TextBook,
+  TitleContainer,
+} from "./styles";
 import image from "./../../Assets/hobit.png";
 
 const MyBooks: React.FC = () => {
-
-    return (
-      <>
-        <ContainerMyBook contentContainerStyle={{height: 375}} horizontal>
-          <ContainerLinkBook>
-            <TouchableOpacity>
-              <Image source={image} />
-              <Text>O Hobbit - J. R. R. Tolkien</Text>
-            </TouchableOpacity>
-          </ContainerLinkBook>
-          <ContainerLinkBook>
-            <TouchableOpacity>
-              <Image source={image} />
-              <Text>O Hobbit - J. R. R. Tolkien</Text>
-            </TouchableOpacity>
-          </ContainerLinkBook>
-          <ContainerLinkBook>
-            <TouchableOpacity>
-              <Image source={image} />
-              <Text>O Hobbit - J. R. R. Tolkien</Text>
-            </TouchableOpacity>
-          </ContainerLinkBook>
-        </ContainerMyBook>
-      </>
-    );
-
+  return (
+    <>
+      <TitleContainer>
+        <Text style={{color: '#fff', fontSize: 24, fontWeight: '800'}}>Meus Livros</Text>
+        <TouchableOpacity><Text style={{color: '#505763'}}>ver mais (122)</Text></TouchableOpacity>
+        
+      </TitleContainer>
+      <ContainerMyBook
+        contentContainerStyle={{ height: 330 }}
+        style={{ maxHeight: 330 }}
+        horizontal
+      >
+        <ContainerLinkBook>
+          <TouchableOpacity>
+            <ImageBook resizeMode="center" source={image} />
+            <TextBook>O Hobbit - J. R. R. Tolkien</TextBook>
+          </TouchableOpacity>
+        </ContainerLinkBook>
+        <ContainerLinkBook>
+          <TouchableOpacity>
+            <ImageBook resizeMode="center" source={image} />
+            <TextBook>O Hobbit - J. R. R. Tolkien</TextBook>
+          </TouchableOpacity>
+        </ContainerLinkBook>
+        <ContainerLinkBook>
+          <TouchableOpacity>
+            <ImageBook resizeMode="center" source={image} />
+            <TextBook>O Hobbit - J. R. R. Tolkien</TextBook>
+          </TouchableOpacity>
+        </ContainerLinkBook>
+      </ContainerMyBook>
+    </>
+  );
 };
 
 export default MyBooks;
--- src/Components/MyBooks/styles.ts
+++ src/Components/MyBooks/styles.ts
@@ -1,13 +1,37 @@
 import styled from "styled-components/native";
 
+export const TitleContainer = styled.View`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    margin-top: 28px;
+    margin-left: 16px;
+    margin-right: 16px;
+`;
+
 export const ContainerMyBook = styled.ScrollView`
-    background: #262626;
+    height: 330px;
     width: 100%;
-    margin-top: 34px;
+    padding: 0 16px;
+    margin-top: 24px;
 `;
 
 export const ContainerLinkBook = styled.View`
     display: flex;
-    height: 100%;
+    align-items: center;
+    width: 200px;
+    margin-right: 16px;
+    flex-direction: column;
+`;
+
+export const ImageBook = styled.Image`
+    height: 300px;
+`;
+
+export const TextBook = styled.Text`
+    color: #787B86;
+    text-align: center;
+    margin-top: 10px;
 `;
 // export const ContainerBook = styled.View``;
